c46b8030a910f4781afa49ee5d6e7ce9333f359a
Erin Drummond <erin.dru@gmail.com>
2014-07-14 22:54:36 +1200

    The site is basically functional

--- js/site.js
+++ js/site.js
@@ -1,3 +1,161 @@
 (function($) {
 
+	/* Hero image fader */
+	$(document).ready(function() {
+		$("#hero .dots .dot").click(function() {
+			$("#hero .dots .active").removeClass("active");
+			var li = $(this).closest("li");
+			$(li).addClass("active");
+			$("#hero .images .active").removeClass("active");
+			$("#hero .images li").eq($(li).index()).addClass("active");
+		});
+
+		setInterval(function() {
+			$("#hero .dots li.active").nextWrap().find(".dot").click();
+		}, 3500);
+	});
+
+	/* Work item gallery */
+	$(document).ready(function() {
+
+		$("#work .gallery li").click(function(evt) {
+			var src = $(this).find("img").attr("src");
+			$("#work-show").find("img").attr("src", src);
+			$("#work-show").addClass("active");
+			evt.stopPropagation();
+		});
+
+		$("#work-show").click(function() {
+			$("#work-show").removeClass("active");
+		});
+
+		$("#work .gallery .next").click(function() {
+			var row1_actives = $("#work .gallery .row1 .active");
+			var row2_actives = $("#work .gallery .row2 .active");
+
+			if ($(row1_actives).last().length) {
+				$(row1_actives).first().removeClass("active");
+				$(row1_actives).last().next().addClass("active")
+			}
+
+			if ($(row2_actives).last().length) {
+				$(row2_actives).first().removeClass("active");
+				$(row2_actives).last().next().addClass("active")
+			}
+
+			checkButtons();
+		});
+
+		$("#work .gallery .prev").click(function() {
+			var row1_actives = $("#work .gallery .row1 .active");
+			var row2_actives = $("#work .gallery .row2 .active");
+
+			if ($(row1_actives).first().length) {
+				$(row1_actives).last().removeClass("active");
+				$(row1_actives).first().prev().addClass("active")
+			}
+
+			if ($(row2_actives).first().length) {
+				$(row2_actives).last().removeClass("active");
+				$(row2_actives).first().prev().addClass("active")
+			}
+
+			checkButtons();
+		});
+
+		function checkButtons() {
+			var hide_prev = $("#work .gallery .row.row1 li").first().is(".active");
+			var hide_next = $("#work .gallery .row.row1 li").last().is(".active");
+
+			if (hide_prev) {
+				$("#work .gallery .prev").hide();
+			} else {
+				$("#work .gallery .prev").show();
+			}
+
+			if (hide_next) {
+				$("#work .gallery .next").hide();
+			} else {
+				$("#work .gallery .next").show();
+			}
+		}
+
+		checkButtons();
+
+	});
+
+	/* Contact Form */
+	$(document).ready(function() {
+		$("#contact .form .submit input").click(function() {
+			var name = $("#contact .form input[name='name']").val();
+			var last_name = $("#contact .form input[name='last_name']").val();
+			var tel_no = $("#contact .form input[name='tel_no']").val();
+			var email = $("#contact .form input[name='email']").val();
+			var message = $("#contact .form textarea").val();
+
+			var subject = "Stonecraft Contact Form Submission";
+			var body = "Name: " + name + "<br />" +
+					   "Last Name: " + last_name + "<br />" +
+					   "Telephone Number: " + tel_no + "<br />" +
+					   "Email Address: " + email + "<br />" +
+					   "Message: " + message + "<br />";
+
+			if (!name || !email) {
+				alert("Please enter a name and and an email address");
+				return;
+			}
+
+			var mandrill_api_key = "bNzYovZ1dGIrWQk6g4zAow"; //use it to send spam. go on, I dare you.
+
+			var submit = this;
+			$(submit).val("Submitting").attr("disabled", "disabled");
+
+			$.post("https://mandrillapp.com/api/1.0/messages/send.json", {
+				"key" : mandrill_api_key,
+				"message" : {
+					"subject" : subject,
+					"html" : body,
+					"from_email" : "noreply@stonecraft4u.co.nz",
+					"from_name" : "Stonecraft Website",
+					"to" : [{
+						"email" : "enquiries@stonecraft4u.co.nz"
+					}]
+				}
+			}, function(data) {
+				if (data[0].status != "sent") {
+					alert("Error: Unable to send email: " + data[0].reject_reason);
+					$(submit).removeAttr("disabled").val("Submit");
+				} else {
+					$("#contact").addClass("sent");
+					$(submit).hide();
+				}
+			});
+
+		});
+	});
+
 })(jQuery);
+
+/* Stolen from http://darklaunch.com/2013/08/06/jquery-next-prev-with-wrapping */
+
+(function( $ ) {
+    $.fn.nextWrap = function( selector ) {
+        var $next = $(this).next( selector );
+
+        if ( ! $next.length ) {
+            $next = $(this).parent().children( selector ).first();
+        }
+
+        return $next;
+    };
+
+    $.fn.prevWrap = function( selector ) {
+        var $previous = $(this).prev( selector );
+
+        if ( ! $previous.length ) {
+            $previous = $(this).parent().children( selector ).last();
+        }
+
+        return $previous;
+    };
+})( jQuery );
